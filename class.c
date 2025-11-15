6f06861310a8df94eb6fa1673e8e0dba7be31b47
poornima <poornima200828@gmail.com>
2025-11-15 21:25:14 +0530

    to find the largest word in a sentence
    yes I did this!! hee hee

--- class.c
+++ class.c
@@ -1,14 +1,57 @@
 #include <stdio.h>
-int main(){
-	int a,b,c,d;
-	printf("enter the value a, b,c,d:");
-	scanf("%d %d %d %d",&a,&b,&c,&d);
-	if(a>b && a>c && a>d){
-	printf("%d =(the first value) Enter is greater",a);
-	}
-	else{
-	printf("the first value=%d is not greater",a);
-	}
-
+#include <string.h>
+void longest_char(char n[100]);
+int main()
+{
+    char n[100]="hell da thamdfsfsbi paya fdggg";
+    longest_char(n);
+}
+void longest_char(char n[100]){
+ int i=0;
+ int large_count=-1;
+ int total_it=0;
+ int space=0;
+ 
+ another_word:
+  int count=0;
+ while(n[i]!='\0'){
+     i++;
+     count++;
+     total_it++;
+     if(large_count<count){
+         large_count=count;
+     }
+     
+     if(n[i]==' '){
+         space++;
+         goto another_word;
+     }
 
 }
+
+
+int j=0;
+int l=0;
+again:
+ int mount=0;
+while(n[j]!='\0'){
+    j++;
+     mount++;
+     l=l+1;
+    if(mount==large_count){
+         break;
+    }
+    if(n[j]==' '){
+        goto again;
+    }
+    
+}
+int last_char=j;
+int first_char=j - large_count;
+for(int k=first_char;k<=last_char;k++){
+    printf("%c",n[k]);
+}
+
+}
+
+
