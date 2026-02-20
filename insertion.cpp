c46f96f116073c5fd87eee372b8426105236f3ad
poornima <poornima200828@gmail.com>
2026-02-20 11:16:40 +0530

    Modify insertion sort to handle larger array

--- insertion.cpp
+++ insertion.cpp
@@ -1,17 +1,20 @@
 #include <iostream>
 using namespace std;
 int main() 
-{    int arr[5]={4,5,6,3,2};
-	for(int i=0;i<5;i++){
-       for(int j=i;j>=0;j--){
-            if(arr[i]>arr[j]){
+{    int arr[8]={4,5,6,3,2,44,67,89};
+	for(int i=0;i<8;i++){
+       for(int j=i-1;j>=0;j--){
+            if(arr[j]<arr[j+1]){
               int temp;
-			  temp=arr[i];
-			  arr[i]=arr[j];
-			  arr[j]=temp;
+			  temp=arr[j];
+			  arr[j]=arr[j+1];
+			  arr[j+1]=temp;
 			}
 	   }
-	}for(int i=0;i<5;i++){
-		cout<<arr[i];
+	}
+	for(int i=0;i<5;i++){
+		cout<<arr[i]<<" ";
+	
+	
 	}
 }
